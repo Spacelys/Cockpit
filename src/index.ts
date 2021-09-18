0028f1fc591f96fd254f79d0a70345ce049bf0aa
Alan Fernandez <Alanf718@gmail.com>
2021-09-17 20:58:09 -0400

    quick export fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {mouse, MouseInput} from './input/mouse';
+export {mouse, MouseInput, MouseWheel} from './input/mouse';
 export {keys, KeyInput} from './input/keys';
 export {gamepad, GamePadInput, GamepadInputDirection, GamePadCockpit} from './input/gamepad';
